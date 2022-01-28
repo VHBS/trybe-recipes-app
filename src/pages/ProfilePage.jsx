50925f04644580ce00094d425841cc9ce5b920a5
mayandronico <mayandronico@msn.com>
2022-01-28 15:56:21 -0300

    Começa Profile Page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+export default function ProfilePage() {
+
+return(
+    <input
+    name="email"
+    type="email"
+    >
+    </input>
+)
+}
+
+// //Todos o data-testid do email e de todos os botões
+// O elemento de email deve possuir o atributo data-testid="profile-email";
+// O botão para "Done Recipes" deve possuir o atributo data-testid="profile-done-btn";
+// O botão para "Favorite Recipes" deve possuir o atributo data-testid="profile-favorite-btn";
+// O botão de "Logout" deve possuir o atributo data-testid="profile-logout-btn".

f146ac531240566dea0b77deb923cd94e6976a79
Eliana Nunes <elianadnunes@gmail.com>
2022-01-28 18:43:29 -0200

    Requisitos 22,23 e 24 footer

--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 export default function FooterMenu() {
+  const history = useHistory();
+
+  const redirectToDrinks = () => {
+    history.push('/drinks');
+  };
+
+  const redirectToExplore = () => {
+    history.push('/explore');
+  };
+
+  const redirectToFoods = () => {
+    history.push('/foods');
+  };
+
   return (
     <footer id="footer" data-testid="footer">
-      <button type="button" data-testid="drinks-bottom-btn" src={ drinkIcon }>
+      <button
+        type="button"
+        data-testid="drinks-bottom-btn"
+        src={ drinkIcon }
+        onClick={ redirectToDrinks }
+      >
         <img src={ drinkIcon } alt="icone para bebidas" />
       </button>
-      <button type="button" data-testid="explore-bottom-btn" src={ exploreIcon }>
+      <button
+        type="button"
+        data-testid="explore-bottom-btn"
+        src={ exploreIcon }
+        onClick={ redirectToExplore }
+      >
         <img src={ exploreIcon } alt="icone para exploracao" />
       </button>
-      <button type="button" data-testid="food-bottom-btn" src={ mealIcon }>
+      <button
+        type="button"
+        data-testid="food-bottom-btn"
+        src={ mealIcon }
+        onClick={ redirectToFoods }
+      >
         <img src={ mealIcon } alt="icone para comidas" />
       </button>
     </footer>

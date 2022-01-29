69387757ec0569abff6a31411547c7485e634489
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-01-29 13:13:16 -0300

    Update FooterMenu.jsx

--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -18,7 +18,7 @@ export default function FooterMenu() {
   const redirectToFoods = () => {
     history.push('/foods');
   };
-  
+ 
   return (
     <footer id="footer" data-testid="footer">
       <button

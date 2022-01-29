09f0ddee239d961f9c7703e57ad08a95537c3609
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-01-29 15:19:12 -0300

    req: 25 ao 27 - implementa tela principal, quantidade certa de itens e botoes de categoria

--- src/components/CardRecepies.jsx
+++ src/components/CardRecepies.jsx
@@ -1,13 +1,48 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import RecepiesContext from '../context/RecepiesContext';
 
 const MAX_QUANTITY = 12;
+const PRINCIPAL_FOOD_API = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const PRINCIPAL_DRINK_API = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+const FIVE_FILTER_FOOD = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+const FIVE_FILTER_DRINK = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+const FILTER_SIZE = 5;
 
 export default function CardRecepies() {
-  const { resultAPI } = useContext(RecepiesContext);
+  const { resultAPI,
+    setResultAPI, fiveFilter, setFiveFilter } = useContext(RecepiesContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    const requisitionAPI = (url, setState) => {
+      fetch(`${url}`)
+        .then((response) => response.json())
+        .then((data) => setState(data));
+    };
+    if (history.location.pathname === '/foods') {
+      requisitionAPI(FIVE_FILTER_FOOD, setFiveFilter);
+      return requisitionAPI(PRINCIPAL_FOOD_API, setResultAPI);
+    }
+    requisitionAPI(FIVE_FILTER_DRINK, setFiveFilter);
+    return requisitionAPI(PRINCIPAL_DRINK_API, setResultAPI);
+  }, [history.location.pathname, setFiveFilter, setResultAPI]);
+
+  const fiveFilterMap = (array) => array.map((item, index) => (
+    <button
+      data-testid={ `${item.strCategory}-category-filter` }
+      type="button"
+      key={ item.strCategory + index }
+    >
+      {item.strCategory}
+    </button>
+  ));
 
   return (
     <div>
+      {fiveFilter.meals && fiveFilterMap(fiveFilter.meals.slice(0, FILTER_SIZE))}
+
       {resultAPI.meals && resultAPI.meals.slice(0, MAX_QUANTITY).map((item, index) => ((
         <div data-testid={ `${index}-recipe-card` } key={ item.idMeal }>
           <p>{item.idMeal}</p>
@@ -19,6 +54,9 @@ export default function CardRecepies() {
           />
         </div>)
       ))}
+
+      {fiveFilter.drinks && fiveFilterMap(fiveFilter.drinks.slice(0, FILTER_SIZE))}
+
       {resultAPI.drinks && resultAPI.drinks.slice(0, MAX_QUANTITY).map((item, index) => ((
         <div data-testid={ `${index}-recipe-card` } key={ item.idDrink }>
           <p>{item.idDrink}</p>
--- src/context/RecepiesProvider.jsx
+++ src/context/RecepiesProvider.jsx
@@ -4,8 +4,9 @@ import RecepiesContext from './RecepiesContext';
 
 export default function Provider({ children }) {
   const [resultAPI, setResultAPI] = useState([]);
+  const [fiveFilter, setFiveFilter] = useState([]);
 
-  const context = { resultAPI, setResultAPI };
+  const context = { resultAPI, setResultAPI, fiveFilter, setFiveFilter };
   return (
     <RecepiesContext.Provider value={ context }>
       {children}

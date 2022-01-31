076c88a32c979969c8cc04b8bde80c153a8c6286
mayandronico <mayandronico@msn.com>
2022-01-31 18:52:55 -0300

    Requisitos 70, 71, 72 e 73

--- src/pages/ExplorerDrink.jsx
+++ src/pages/ExplorerDrink.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 
 export default function ExplorerDrink() {
+  const history = useHistory();
+
+  const redirectToExpDrink = () => {
+    history.push('/explore/drinks/ingredients');
+  };
+
+  const redirectToDrinkDetails = () => {
+    history.push('/drinks/:id');
+  };
   return (
     <div>
       <Header title="Explore Drinks" />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+        onClick={ redirectToExpDrink }
+      >
+        By Ingredient
+      </button>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+        onClick={ redirectToDrinkDetails }
+      >
+        Surprise me!
+      </button>
       <FooterMenu />
     </div>);
 }

31073532616d1f14fcc82b1de0e2ede21edcd6b1
mayandronico <mayandronico@msn.com>
2022-01-31 16:26:17 -0300

    Requisitos 67,68 e 69

--- src/pages/ExplorerPage.jsx
+++ src/pages/ExplorerPage.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 
 export default function ExplorerPage() {
+  const history = useHistory();
+
+  const redirectToFoods = () => {
+    history.push('/explore/foods');
+  };
+
+  const redirectToDrinks = () => {
+    history.push('/explore/drinks');
+  };
+
   return (
     <div>
       <Header title="Explore" />
+      <button
+        data-testid="explore-foods"
+        type="button"
+        onClick={ redirectToFoods }
+      >
+        Explore Foods
+      </button>
+      <button
+        data-testid="explore-drinks"
+        type="button"
+        onClick={ redirectToDrinks }
+      >
+        Explore Drinks
+      </button>
       <FooterMenu />
     </div>
   );

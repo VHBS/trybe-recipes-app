0b10eed10de19109921de893e04ad9f4fd9ddb25
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-02-03 16:43:56 -0300

    req: 47.1 - renderiza os itens da receita em progresso - food

--- src/pages/FoodInProgress.jsx
+++ src/pages/FoodInProgress.jsx
@@ -1,8 +1,74 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import FavoriteButton from '../components/FavoriteButton';
+import ShareButton from '../components/ShareButton';
+import RecepiesContext from '../context/RecepiesContext';
+
+const DETAILS_FOOD_API = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+const INGREDIENT_QUANTITY = 20;
 
 export default function FoodInProgress() {
+  const { detailProduct, setDetailProduct } = useContext(RecepiesContext);
+  const [ingredients, setIngredients] = useState([]);
+  const { id } = useParams();
+  let product;
+
+  useEffect(() => {
+    const requisitionById = (url, value) => {
+      fetch(`${url}${value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setDetailProduct(data);
+          const ingredientsArr = [];
+          for (let i = 1; i < INGREDIENT_QUANTITY; i += 1) {
+            const igred = data.meals[0][`strIngredient${i}`];
+            const quantity = data.meals[0][`strMeasure${i}`];
+            if (igred !== '' && igred !== null && igred !== undefined) {
+              ingredientsArr.push(`${igred} - ${quantity}`);
+            }
+          }
+          setIngredients(ingredientsArr);
+        })
+        .catch(() => []);
+    };
+
+    requisitionById(DETAILS_FOOD_API, id);
+  }, [id, setDetailProduct]);
+
+  if (detailProduct.meals) {
+    const [firstItem] = detailProduct.meals;
+    product = firstItem;
+  }
+
   return (
     <div>
-      <h2>Progress</h2>
+      {product && (
+        <>
+          <img
+            data-testid="recipe-photo"
+            src={ product.strMealThumb }
+            alt={ product.strMeal }
+          />
+          <h2 data-testid="recipe-title">{product.strMeal}</h2>
+          <ShareButton />
+          <FavoriteButton />
+          <p data-testid="recipe-category">{product.strCategory}</p>
+          { ingredients.map((item, index) => (
+            <p
+              data-testid={ `${index}-ingredient-step` }
+              key={ item + index }
+            >
+              {item}
+            </p>
+          )) }
+          <p data-testid="instructions">{product.strInstructions}</p>
+          <button
+            data-testid="finish-recipe-btn"
+            type="button"
+          >
+            Finish Recipe
+          </button>
+        </>
+      )}
     </div>);
 }
